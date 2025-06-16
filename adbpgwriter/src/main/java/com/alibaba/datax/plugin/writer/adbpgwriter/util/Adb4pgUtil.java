c363adc218b75b6f6390117ff77880d0a8503979
zz <ze.zhang@transwarp.io>
2025-06-16 14:23:33 +0800

    DESC:Format some classes.

--- adbpgwriter/src/main/java/com/alibaba/datax/plugin/writer/adbpgwriter/util/Adb4pgUtil.java
+++ adbpgwriter/src/main/java/com/alibaba/datax/plugin/writer/adbpgwriter/util/Adb4pgUtil.java
@@ -27,120 +27,122 @@ import static com.alibaba.datax.plugin.rdbms.util.DBUtilErrorCode.COLUMN_SPLIT_E
  */
 public class Adb4pgUtil {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Adb4pgUtil.class);
-    private static final DataBaseType DATABASE_TYPE = DataBaseType.PostgreSQL;
-    public static void checkConfig(Configuration originalConfig) {
-        try {
+  private static final Logger LOG = LoggerFactory.getLogger(Adb4pgUtil.class);
+  private static final DataBaseType DATABASE_TYPE = DataBaseType.PostgreSQL;
 
-            DatabaseConfig databaseConfig = convertConfiguration(originalConfig);
+  public static void checkConfig(Configuration originalConfig) {
+    try {
 
-            Adb4pgClient testConfigClient = new Adb4pgClient(databaseConfig);
-        } catch (Exception e) {
-            throw new Adb4pgClientException(Adb4pgClientException.CONFIG_ERROR, "Check config exception: " + e.getMessage(), null);
-        }
+      DatabaseConfig databaseConfig = convertConfiguration(originalConfig);
+
+      Adb4pgClient testConfigClient = new Adb4pgClient(databaseConfig);
+    } catch (Exception e) {
+      throw new Adb4pgClientException(Adb4pgClientException.CONFIG_ERROR, "Check config exception: " + e.getMessage(), null);
     }
+  }
 
-    public static DatabaseConfig convertConfiguration(Configuration originalConfig) {
-        originalConfig.getNecessaryValue(Key.USERNAME, COLUMN_SPLIT_ERROR);
-        originalConfig.getNecessaryValue(Key.PASSWORD, COLUMN_SPLIT_ERROR);
+  public static DatabaseConfig convertConfiguration(Configuration originalConfig) {
+    originalConfig.getNecessaryValue(Key.USERNAME, COLUMN_SPLIT_ERROR);
+    originalConfig.getNecessaryValue(Key.PASSWORD, COLUMN_SPLIT_ERROR);
 
 
-        String userName = originalConfig.getString(Key.USERNAME);
-        String passWord = originalConfig.getString(Key.PASSWORD);
-        String tableName = originalConfig.getString(Key.TABLE);
-        String schemaName = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.SCHEMA);
-        String host = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.HOST);
-        String port = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.PORT);
-        String databseName = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.DATABASE);
+    String userName = originalConfig.getString(Key.USERNAME);
+    String passWord = originalConfig.getString(Key.PASSWORD);
+    String tableName = originalConfig.getString(Key.TABLE);
+    String schemaName = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.SCHEMA);
+    String host = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.HOST);
+    String port = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.PORT);
+    String databseName = originalConfig.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.DATABASE);
 
-        List<String> columns = originalConfig.getList(Key.COLUMN, String.class);
-        DatabaseConfig databaseConfig = new DatabaseConfig();
-        databaseConfig.setHost(host);
-        databaseConfig.setPort(Integer.valueOf(port));
-        databaseConfig.setDatabase(databseName);
+    List<String> columns = originalConfig.getList(Key.COLUMN, String.class);
+    DatabaseConfig databaseConfig = new DatabaseConfig();
+    databaseConfig.setHost(host);
+    databaseConfig.setPort(Integer.valueOf(port));
+    databaseConfig.setDatabase(databseName);
 
-        databaseConfig.setUser(userName);
+    databaseConfig.setUser(userName);
 
-        databaseConfig.setPassword(passWord);
-        databaseConfig.setLogger(LOG);
+    databaseConfig.setPassword(passWord);
+    databaseConfig.setLogger(LOG);
 
-        databaseConfig.setInsertIgnore(originalConfig.getBool(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.IS_INSERTINGORE, true));
-        databaseConfig.addTable(Collections.singletonList(tableName), schemaName);
-        databaseConfig.setColumns(columns, tableName, schemaName);
+    databaseConfig.setInsertIgnore(originalConfig.getBool(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.IS_INSERTINGORE, true));
+    databaseConfig.addTable(Collections.singletonList(tableName), schemaName);
+    databaseConfig.setColumns(columns, tableName, schemaName);
 
-        return databaseConfig;
-    }
+    return databaseConfig;
+  }
 
-    private static Map<String, List<String>> splitBySchemaName(List<String> tables) {
-        HashMap<String, List<String>> res = new HashMap<String, List<String>>(16);
+  private static Map<String, List<String>> splitBySchemaName(List<String> tables) {
+    HashMap<String, List<String>> res = new HashMap<String, List<String>>(16);
 
-        for (String schemaNameTableName: tables) {
-            String[] s = schemaNameTableName.split("\\.");
-            if (!res.containsKey(s[0])) {
-                res.put(s[0], new ArrayList<String>());
-            }
-            res.get(s[0]).add(s[1]);
-
-        }
-
-        return res;
-    }
-
-    public static Connection getAdbpgConnect(Configuration conf) {
-        String userName = conf.getString(Key.USERNAME);
-        String passWord = conf.getString(Key.PASSWORD);
-
-        return DBUtil.getConnection(DataBaseType.PostgreSQL, generateJdbcUrl(conf), userName, passWord);
+    for (String schemaNameTableName : tables) {
+      String[] s = schemaNameTableName.split("\\.");
+      if (!res.containsKey(s[0])) {
+        res.put(s[0], new ArrayList<String>());
+      }
+      res.get(s[0]).add(s[1]);
 
     }
 
-    private static String generateJdbcUrl(Configuration configuration) {
-        String host = configuration.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.HOST);
-        String port = configuration.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.PORT);
-        String databseName = configuration.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.DATABASE);
-        String jdbcUrl = "jdbc:postgresql://" + host + ":" + port + "/" + databseName;
-        return jdbcUrl;
+    return res;
+  }
 
+  public static Connection getAdbpgConnect(Configuration conf) {
+    String userName = conf.getString(Key.USERNAME);
+    String passWord = conf.getString(Key.PASSWORD);
+
+    return DBUtil.getConnection(DataBaseType.PostgreSQL, generateJdbcUrl(conf), userName, passWord);
+
+  }
+
+  private static String generateJdbcUrl(Configuration configuration) {
+    String host = configuration.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.HOST);
+    String port = configuration.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.PORT);
+    String databseName = configuration.getString(com.alibaba.datax.plugin.writer.adbpgwriter.util.Key.DATABASE);
+    String jdbcUrl = "jdbc:postgresql://" + host + ":" + port + "/" + databseName;
+    return jdbcUrl;
+
+  }
+
+  public static void prepare(Configuration originalConfig) {
+    List<String> preSqls = originalConfig.getList(Key.PRE_SQL,
+        String.class);
+
+    String tableName = originalConfig.getString(Key.TABLE);
+    List<String> renderedPreSqls = WriterUtil.renderPreOrPostSqls(
+        preSqls, tableName);
+
+    if (renderedPreSqls.size() == 0) {
+      return;
     }
-    public static void prepare(Configuration originalConfig) {
-        List<String> preSqls = originalConfig.getList(Key.PRE_SQL,
-                String.class);
 
-        String tableName = originalConfig.getString(Key.TABLE);
-        List<String> renderedPreSqls = WriterUtil.renderPreOrPostSqls(
-                preSqls, tableName);
+    originalConfig.remove(Key.PRE_SQL);
 
-        if (renderedPreSqls.size() == 0) {
-            return;
-        }
-
-        originalConfig.remove(Key.PRE_SQL);
-
-        Connection conn = getAdbpgConnect(originalConfig);
-        WriterUtil.executeSqls(conn, renderedPreSqls, generateJdbcUrl(originalConfig), DATABASE_TYPE);
-        DBUtil.closeDBResources(null, null, conn);
+    Connection conn = getAdbpgConnect(originalConfig);
+    WriterUtil.executeSqls(conn, renderedPreSqls, generateJdbcUrl(originalConfig), DATABASE_TYPE);
+    DBUtil.closeDBResources(null, null, conn);
 
 
+  }
+
+  public static void post(Configuration configuration) {
+    List<String> postSqls = configuration.getList(Key.POST_SQL,
+        String.class);
+    String tableName = configuration.getString(Key.TABLE);
+    List<String> renderedPostSqls = WriterUtil.renderPreOrPostSqls(
+        postSqls, tableName);
+
+    if (renderedPostSqls.size() == 0) {
+      return;
     }
 
-    public static void post(Configuration configuration) {
-        List<String> postSqls = configuration.getList(Key.POST_SQL,
-                String.class);
-        String tableName = configuration.getString(Key.TABLE);
-        List<String> renderedPostSqls = WriterUtil.renderPreOrPostSqls(
-                postSqls, tableName);
+    configuration.remove(Key.POST_SQL);
 
-        if (renderedPostSqls.size() == 0) {
-            return;
-        }
+    Connection conn = getAdbpgConnect(configuration);
 
-        configuration.remove(Key.POST_SQL);
-
-        Connection conn =  getAdbpgConnect(configuration);
-
-        WriterUtil.executeSqls(conn, renderedPostSqls, generateJdbcUrl(configuration), DATABASE_TYPE);
-        DBUtil.closeDBResources(null, null, conn);
-    }
+    WriterUtil.executeSqls(conn, renderedPostSqls, generateJdbcUrl(configuration), DATABASE_TYPE);
+    DBUtil.closeDBResources(null, null, conn);
+  }
 
 
 }
